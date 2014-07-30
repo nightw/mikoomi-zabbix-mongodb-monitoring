df303a3a234e892d61ba44dc1b56a30501934117
Pal David Gergely <nightw17@gmail.com>
2014-07-30 18:35:35 +0200

    Added some extra PATH values to the .sh file to make sure it will find all the needed executables

--- mikoomi-mongodb-plugin.sh
+++ mikoomi-mongodb-plugin.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-PATH=$PATH:/etc/zabbix/externalscripts:/home/zabbix/bin:/bin:/usr/sbin:/usr/bin:/usr/local/bin:/usr/local/sbin
+PATH=$PATH:/etc/zabbix/externalscripts:/opt/zabbix/externalscripts:/opt/zabbix/bin:/home/zabbix/bin:/bin:/usr/sbin:/usr/bin:/usr/local/bin:/usr/local/sbin
 export PATH
 BASE_DIR="`dirname $0`"
 /usr/bin/php $BASE_DIR/mikoomi-mongodb-plugin.php $* 
