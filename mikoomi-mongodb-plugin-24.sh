be09af7e1d7aec4c26b4fb26aa27650190c88b03
Pal David Gergely <nightw17@gmail.com>
2016-04-07 20:22:50 +0200

    Added the shell script in the Zabbix 2.4 version

---
+++ mikoomi-mongodb-plugin-24.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+PATH=$PATH:/etc/zabbix/externalscripts:/opt/zabbix/externalscripts:/opt/zabbix/bin:/home/zabbix/bin:/bin:/usr/sbin:/usr/bin:/usr/local/bin:/usr/local/sbin
+export PATH
+BASE_DIR="`dirname $0`"
+/usr/bin/php $BASE_DIR/mikoomi-mongodb-plugin-24.php $* 
+echo 0
